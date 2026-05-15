4a78bed52995baaeedd9e7dfd90f4b62f090ab3f
AnhCreG <sharkanhnt1254@gmail.com>
2026-05-15 14:10:37 +0700

    Thêm icon và chỉnh sửa nhẹ

--- app/src/main/java/com/example/mymoney/ui/common/IconMapper.kt
+++ app/src/main/java/com/example/mymoney/ui/common/IconMapper.kt
@@ -4,13 +4,19 @@ import com.example.mymoney.R
 
 fun mapEmojiToDrawable(icon: String): Int {
     return when(icon) {
-        "🍜" -> R.drawable.ic_category_food_noodle
-        "🛍" -> R.drawable.ic_category_shopping_bag
-        "🏠" -> R.drawable.ic_category_mansion
-        "🎮" -> R.drawable.ic_category_joystick
-        "💊" -> R.drawable.ic_category_pills
-        "📚" -> R.drawable.ic_category_graduation_cap
-        "🚗" -> R.drawable.ic_category_car
-        else -> R.drawable.ic_category_food_noodle
+        "🍜" -> R.drawable.ic_category_expense_noodle
+        "🛍" -> R.drawable.ic_category_expense_shopping_bag
+        "🏠" -> R.drawable.ic_category_expense_mansion
+        "🎮" -> R.drawable.ic_category_expense_joystick
+        "💊" -> R.drawable.ic_category_expense_pills
+        "📚" -> R.drawable.ic_category_expense_education
+        "📄" -> R.drawable.ic_category_expense_bill
+        "📦" -> R.drawable.ic_category_expense_box
+        "🚗" -> R.drawable.ic_category_expense_car
+        "💰" -> R.drawable.ic_category_income_money
+        "🎁" -> R.drawable.ic_category_income_bonus
+        "📈" -> R.drawable.ic_category_income_investment
+        "💼" -> R.drawable.ic_category_income_bag
+        else -> R.drawable.ic_category_expense_noodle
     }
 }
